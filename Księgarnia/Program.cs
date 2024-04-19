d51883f6ae5577a1ffd6763cb33fa67cd1bacac4
AdamBolonekTeb <152616806+AdamBolonekTeb@users.noreply.github.com>
2024-04-19 13:25:00 +0200

    Add files via upload

---
+++ Księgarnia/Program.cs
@@ -0,0 +1,62 @@
+﻿using Księgarnia;
+
+void menu(string? message = "")
+{
+    Console.Clear();
+
+    if (!string.IsNullOrEmpty(message)) Console.WriteLine(message);
+    Console.WriteLine("1 Wszystkie Ksiazki");
+    Console.WriteLine("2 Wyswietl wybrana ksiazke");
+    Console.WriteLine("3 Dodaj ksiazke");
+    Console.WriteLine("4 Usun ksiazke");
+
+    int i = Convert.ToInt16(Console.ReadLine());
+
+    switch (i)
+    {
+        default:
+            menu();
+            break;
+
+        case 1:
+            Ksiazka.ShowAll();
+            Console.ReadKey();
+
+            menu();
+            break;
+
+        case 2:
+            Console.WriteLine("Podaj id ksiazki, ktora chcesz wyswietlic");
+            Ksiazka.Show(Convert.ToInt16(Console.ReadLine()));
+            Console.ReadKey();
+
+            menu();
+            break;
+
+        case 3:
+            Console.WriteLine("Tytul Ksiazki");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Autor ksiazki");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Rok wydania ksiazki");
+            int releaseDate = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Gatunek ksiazki");
+            string genre = Console.ReadLine();
+
+            Ksiazka.Add(title, author, releaseDate, genre);
+
+            menu("Ksiazka zostala dodana poprawnie");
+            break;
+
+        case 4:
+            Console.WriteLine("Daj id ksiazki, ktora ma zostac usunieta");
+            Ksiazka.Remove(Convert.ToInt16(Console.ReadLine()));
+
+            menu("Ksiezke udalo sie usunac poprawnie");
+            break;
+    }
+}
+menu();
---
+++ Księgarnia/książki.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Księgarnia
+{
+    public class Ksiazka
+    {
+        static List<Ksiazka>? ksiazki = new List<Ksiazka>();
+        static string path = "ksiazki.json";
+
+        static Ksiazka() => RefreshData();
+
+        public static void RefreshData() =>
+          ksiazki = JsonSerializer.Deserialize<List<Ksiazka>>(File.ReadAllText(path));
+
+        public static bool Exists(int id)
+        {
+            return ksiazki.Any(k => k.Id == id);
+        }
+
+        public static void Info(Ksiazka k)
+        {
+            Console.WriteLine($"{k.Id} | {k.Tytul}");
+            Console.WriteLine(k.Gatunek);
+            Console.WriteLine(k.Autor);
+            Console.WriteLine(k.RokWydania);
+        }
+
+        public static void Add(string tytul, string autor, int rokWydania, string gatunek)
+        {
+            ksiazki.Add(
+              new Ksiazka
+              {
+                  Id = ksiazki.Count + 1,
+                  Tytul = tytul,
+                  Autor = autor,
+                  RokWydania = rokWydania,
+                  Gatunek = gatunek,
+              });
+
+            File.WriteAllText(path, JsonSerializer.Serialize(ksiazki));
+        }
+
+        public static void Remove(int id)
+        {
+            if (!Exists(id))
+            {
+                Console.WriteLine("Nie ma takiej ksiazki");
+                return;
+            }
+
+            ksiazki.RemoveAll((k) => k.Id == id);
+            NormalizeIds();
+
+            File.WriteAllText(path, JsonSerializer.Serialize(ksiazki));
+        }
+
+        public static void NormalizeIds() =>
+          ksiazki.ForEach(k => k.Id = ksiazki.IndexOf(k) + 1);
+
+        
+
+        public static void ShowAll()
+        {
+            ksiazki.ForEach(k =>
+            {
+                Info(k);
+                Console.WriteLine("--------------------");
+            });
+        }
+
+        public int Id { get; set; }
+        public string? Tytul { get; set; }
+        public string? Autor { get; set; }
+        public int RokWydania { get; set; }
+        public string? Gatunek { get; set; }
+
+        public static void Show(int id)
+        /*************************************
+         * Kod sprawdza czy znajduje się taka książka w bazie
+         *************************************/
+        {
+            if (!Exists(id))
+            {
+                Console.WriteLine("Nie ma takiej ksiazki");
+                return;
+            }
+            Ksiazka k = ksiazki.Find(k => k.Id == id);
+
+            Info(k);
+        }
+
+        
+    }
+}
